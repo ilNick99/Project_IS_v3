145d6828c29f7c43eb8458c1e089d1b4716789c9
Lange99 <christianlangellotti@gmail.com>
2021-06-23 17:29:44 +0200

    possibile switch fra i mod

--- src/main/java/Project_v3/Manager.java
+++ src/main/java/Project_v3/Manager.java
@@ -3,6 +3,7 @@ package main.java.Project_v3;
 import main.java.Utility.IO;
 
 import java.io.IOException;
+
 public class Manager {
 
     NetManager manager = new NetManager();
@@ -10,20 +11,24 @@ public class Manager {
     Configurator config = new Configurator();
 
     public void start() throws IOException {
-        int choice = IO.readInteger(IO.CHOOSE_THE_ELEMENT, 0, 2);
-        switch (choice){
-            case 0:
-                break;
+        boolean check=false;
+        do {
+            int choice = IO.readInteger(IO.CHOOSE_THE_ELEMENT, 0, 2);
+            switch (choice) {
+                case 0:
+                    check=false;
+                    break;
+                case 1:
+                    user.operation(manager);
+                    check = IO.yesOrNo("Do you want change the mode?\n");
+                    break;
 
-            case 1:
-                user.operation(manager);
-                break;
-
-            case 2:
-                config.operation(manager);
-                break;
-        }
+                case 2:
+                    config.operation(manager);
+                    check = IO.yesOrNo("Do you want change the mode?\n");
+                    break;
+            }
+        } while (check);
     }
-
 
 }
